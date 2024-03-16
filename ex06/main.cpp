4a0c40df2cd8e69fa4e57c1eb97660df9b299d7e
Sawa <sunzecun@gmail.com>
2024-03-16 19:41:44 +0900

    ERROR: arguments

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -4,8 +4,11 @@ int main(int argc, char *argv[])
 {
 	Harl harl;
 	if (argc != 2)
+	{
 		std::cerr << "Error: the invalid number of arguments" << std::endl;
-	else
-		harl.complain(argv[1]);
+		std::cerr << "[USAGE] ./harlFilter <DEBUG> or <INFO> or <WARNING> or <ERROR>" << std::endl;
+		return (1);
+	}
+	harl.complain(argv[1]);
 	return (0);
 }
